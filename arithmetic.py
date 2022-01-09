6d76e8aee05b9e119f1d5a81ddee7e7ced3454c5
Zuza <zuza.ratajczyk@gmail.com>
2022-01-09 21:53:39 +0100

    Second stage of project

--- arithmetic.py
+++ arithmetic.py
@@ -1,10 +1,7 @@
-math_operation = input()
-first_element = int(math_operation.split()[0])
-second_element = int(math_operation.split()[2])
-input_operator = math_operation.split()[1]
+import random
 
 
-def calculate(first_comp, second_comp,operator):
+def calculate(first_comp, second_comp, operator):
     if operator == '+':
         return first_comp + second_comp
     elif operator == '-':
@@ -13,4 +10,20 @@ def calculate(first_comp, second_comp,operator):
         return first_comp * second_comp
 
 
-print(calculate(first_element, second_element, input_operator))
+def main():
+    first_element = random.randint(2, 9)
+    second_element = random.randint(2, 9)
+    random_operator = random.choice(['+', '-', '*'])
+    print(f"{first_element} {random_operator} {second_element}")
+    user_result = int(input())
+    result = calculate(first_element, second_element, random_operator)
+    if user_result == result:
+        print("Right!")
+    else:
+        print("Wrong!")
+
+
+if __name__ == "__main__":
+    main()
+
+
