7de593eeb038eb2bd6d4436014aea081a2370b98
Zuza Ratajczyk <zuza.ratajczyk@gmail.com>
2022-10-29 19:37:51 +0200

    Third stage of the project - added marks

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,7 @@
 import random
 
+score = 0
+
 
 def calculate(first_comp, second_comp, operator):
     if operator == '+':
@@ -10,17 +12,28 @@ def calculate(first_comp, second_comp, operator):
         return first_comp * second_comp
 
 
-def main():
-    first_element = random.randint(2, 9)
-    second_element = random.randint(2, 9)
-    random_operator = random.choice(['+', '-', '*'])
-    print(f"{first_element} {random_operator} {second_element}")
-    user_result = int(input())
-    result = calculate(first_element, second_element, random_operator)
-    if user_result == result:
-        print("Right!")
+def check_result(user_result, correct_result):
+    global score
+    if user_result == correct_result:
+        score += 1
+        return "Right!"
     else:
-        print("Wrong!")
+        return "Wrong!"
+
+
+def main():
+    for task in range(5):
+        first_element = random.randint(2, 9)
+        second_element = random.randint(2, 9)
+        random_operator = random.choice(['+', '-', '*'])
+        print(f"{first_element} {random_operator} {second_element}")
+        user_result = input()
+        while not user_result.lstrip("-").isdigit():
+            print("Incorrect format.")
+            user_result = input()
+        result = calculate(first_element, second_element, random_operator)
+        print(check_result(int(user_result), result))
+    print(f"Your mark is {score}/5.")
 
 
 if __name__ == "__main__":
