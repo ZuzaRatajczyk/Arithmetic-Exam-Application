f07f6d639fe7ec73e9b5c0b0029619a9dff8d5c6
Zuza Ratajczyk <zuza.ratajczyk@gmail.com>
2022-10-29 21:02:02 +0200

    Fourth stage of the project - added second level and results saving

--- arithmetic.py
+++ arithmetic.py
@@ -1,9 +1,19 @@
 import random
 
-score = 0
+
+def select_level():
+    levels_info = {"1": "simple operations with numbers 2-9", "2": "integral squares of 11-29"}
+    message = f"""Which level do you want? Enter a number:
+    1 - {levels_info["1"]}
+    2 - {levels_info["2"]}
+    """
+    user_level = input(message)
+    while user_level not in levels_info:
+        user_level = input(message)
+    return user_level, levels_info[user_level]
 
 
-def calculate(first_comp, second_comp, operator):
+def calculate_first_level(first_comp, second_comp, operator):
     if operator == '+':
         return first_comp + second_comp
     elif operator == '-':
@@ -12,16 +22,19 @@ def calculate(first_comp, second_comp, operator):
         return first_comp * second_comp
 
 
+def calculate_second_level(number):
+    return number ** 2
+
+
 def check_result(user_result, correct_result):
-    global score
     if user_result == correct_result:
-        score += 1
-        return "Right!"
+        return 1
     else:
-        return "Wrong!"
+        return 0
 
 
-def main():
+def perform_first_level():
+    score = 0
     for task in range(5):
         first_element = random.randint(2, 9)
         second_element = random.randint(2, 9)
@@ -31,9 +44,53 @@ def main():
         while not user_result.lstrip("-").isdigit():
             print("Incorrect format.")
             user_result = input()
-        result = calculate(first_element, second_element, random_operator)
-        print(check_result(int(user_result), result))
-    print(f"Your mark is {score}/5.")
+        result = calculate_first_level(first_element, second_element, random_operator)
+        result_info = check_result(int(user_result), result)
+        if result_info:
+            score += 1
+            print("Right!")
+        else:
+            print("Wrong!")
+    return score
+
+
+def perform_second_level():
+    score = 0
+    for task in range(5):
+        number = random.randint(11, 29)
+        print(number)
+        user_result = input()
+        while not user_result.lstrip("-").isdigit():
+            print("Incorrect format.")
+            user_result = input()
+        result = calculate_second_level(number)
+        result_info = check_result(int(user_result), result)
+        if result_info:
+            score += 1
+            print("Right!")
+        else:
+            print("Wrong!")
+    return score
+
+
+def save_result_to_file(score, level, level_info):
+    name = input("What is your name?")
+    result_file = open("results.txt", "a")
+    result_file.write(f"{name}: {score}/5 in level {level} ({level_info}).")
+    print('The results are saved in "results.txt".')
+    result_file.close()
+
+
+def main():
+    level, level_info = select_level()
+    if level == "1":
+        score = perform_first_level()
+    else:
+        score = perform_second_level()
+    print(f"Your mark is {score}/5. Would you like to save the result? Enter yes or no.")
+    save_result = input()
+    if save_result in ["yes", "y", "YES", "Yes"]:
+        save_result_to_file(score, level, level_info)
 
 
 if __name__ == "__main__":
